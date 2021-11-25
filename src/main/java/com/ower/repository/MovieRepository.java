103779b89cba305a4246b030dcfad956ef5ef92b
owermaiden <ower.maiden@cybertekschool.com>
2021-11-25 13:17:14 +0300

    Data Generator Done

--- src/main/java/com/ower/repository/MovieRepository.java
+++ src/main/java/com/ower/repository/MovieRepository.java
@@ -4,6 +4,10 @@ import com.ower.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+
 }
