f770faac2cdc57a282d7e9b9c78f56035babc2df
owermaiden <ower.maiden@cybertekschool.com>
2021-12-01 09:39:13 +0300

    Query Examples

--- src/main/java/com/ower/CinemaAppApplication.java
+++ src/main/java/com/ower/CinemaAppApplication.java
@@ -1,13 +1,29 @@
 package com.ower;
 
+import com.ower.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CinemaAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CinemaAppApplication.class, args);
+    @Autowired
+    AccountRepository accountRepository;
+
+    @PostConstruct
+    private void testAccount(){
+        System.out.println(accountRepository.findByOrderByAgeDesc());
+
     }
 
+    public static void main(String[] args) { SpringApplication.run(CinemaAppApplication.class, args);
+
+    }
+
+
+
 }
+
--- src/main/java/com/ower/entity/Account.java
+++ src/main/java/com/ower/entity/Account.java
@@ -4,6 +4,7 @@ import com.ower.enums.UserRole;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "account_details")
+@ToString
 public class Account extends BaseEntity {
 
     private String name;
